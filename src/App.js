fee9aa47e1db3639572ded2f150c92809d2967aa
Evgeni Anastasov <one_evg@mail.bg>
2022-10-11 18:53:18 +0300

    added react-router to project

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
 	return (
 		<div id="box">
 
-			<Header/>
+			<Header />
 
 			{/* Main Content */}
 			<main id="main-content">
-				<Home/>
+
+				<Routes>
+
+					<Route path='/' element={<Home />} />
+
+				</Routes>
+
 			</main>
 			{/*Home Page*/}
 
