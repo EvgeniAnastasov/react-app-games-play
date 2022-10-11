ce673403237d24ddfa71aac38da77a068ae474bc
Evgeni Anastasov <one_evg@mail.bg>
2022-10-11 22:49:45 +0300

    show games in category page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 
+import { useEffect, useState } from 'react';
+import * as gameService from './services/gameService'
+
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
@@ -9,6 +12,16 @@ import { Create } from './components/Create/Create';
 import { Catalog } from './components/Catalog/Catalog';
 
 function App() {
+
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		gameService.getAll()
+			.then(result => { setGames(result) })
+	}, [])
+
+
+
 	return (
 		<div id="box">
 
@@ -19,11 +32,11 @@ function App() {
 
 				<Routes>
 
-					<Route path='/' element={<Home />} />
+					<Route path='/' element={<Home games={games}/>} />
 					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
 					<Route path='/create' element={<Create />} />
-					<Route path='/catalog' element={<Catalog />} />
+					<Route path='/catalog' element={<Catalog games={games}/>} />
 
 				</Routes>
 
