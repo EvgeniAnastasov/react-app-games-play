cd60ee3978e96a55e497dc6c32c89df6aeabc0d3
Evgeni Anastasov <one_evg@mail.bg>
2022-10-11 22:20:43 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 
 import { Routes, Route } from 'react-router-dom'
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { Create } from './components/Create/Create';
+import { Catalog } from './components/Catalog/Catalog';
 
 function App() {
 	return (
@@ -16,6 +20,10 @@ function App() {
 				<Routes>
 
 					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/create' element={<Create />} />
+					<Route path='/catalog' element={<Catalog />} />
 
 				</Routes>
 
